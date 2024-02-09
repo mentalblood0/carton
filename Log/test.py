0ddf5bb27673c76dcb7b2d2bee2c30866490184e
mentalblood <neceporenkostepan@gmail.com>
2024-02-09 19:42:54 +0300

    parametrized main benchmark

--- Log/test.py
+++ Log/test.py
@@ -1,20 +1,37 @@
 import itertools
 import sqlite3
 
+import pytest
 import pytest_benchmark.plugin
 
 from .Log import Log, Package
 
 
-def test_benchmark_simple(benchmark: pytest_benchmark.plugin.BenchmarkFixture):
+@pytest.fixture
+def log():
     cursor = sqlite3.connect(":memory:").cursor()
-    log = Log(lambda s: cursor.execute(s).fetchall())
+    return Log(lambda s: cursor.execute(s).fetchall())
 
-    amount = 10000
+
+@pytest.mark.parametrize("amount", [10**n for n in range(6)])
+@pytest.mark.parametrize("selected_key_values_amount", [10**n for n in range(2)])
+@pytest.mark.parametrize("selected_items_amount", [10**n for n in range(2)])
+def test_benchmark_simple(
+    log: Log,
+    benchmark: pytest_benchmark.plugin.BenchmarkFixture,
+    amount: int,
+    selected_key_values_amount: int,
+    selected_items_amount: int,
+):
     log.insert(
         itertools.chain.from_iterable(
-            Package(i, {"key": f"value_{i}", "file": f"path_{i}"}).entries
+            Package(
+                i,
+                {"key": f"value_{i}"}
+                | {"file": f"path_{i}_{j}" for j in range(selected_key_values_amount)},
+            ).entries
             for i in range(amount)
+            for _ in range(selected_items_amount)
         )
     )
 
