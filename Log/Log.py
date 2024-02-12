fa89d9953853b7a701cdc264bf9d03be04a71982
mentalblood <neceporenkostepan@gmail.com>
2024-02-12 21:33:26 +0300

    shorten `Log` class

--- Log/Log.py
+++ Log/Log.py
@@ -1,10 +1,9 @@
 import dataclasses
 import functools
-import itertools
 import typing
 
 
-@dataclasses.dataclass
+@dataclasses.dataclass(unsafe_hash=True)
 class Log:
     execute: typing.Callable[[str], typing.Any]
     execute_enum: typing.Callable[[str], typing.Any]
@@ -28,36 +27,30 @@ class Log:
         self.execute("create index if not exists log_time on log(time)")
         self.execute("create index if not exists log_id on log(id)")
         self.execute("create index if not exists log_value on log(value)")
-        self.execute("create index if not exists log_i_key on log(i, key)")
+        self.execute("create index if not exists log_i_key on log(i,key)")
 
     def insert(self, packages: typing.Iterable[tuple[int | None, dict[str, str]]]):
         buf = []
-        for p in packages:
-            if not p[1]:
-                continue
+        for p in filter(lambda p: p[1], packages):
             if p[0] is not None:
                 buf.extend(f"({p[0]},{self.key_id(k)},'{v}')" for k, v in p[1].items())
             else:
                 rows = [(p[0], k, str(v)) for k, v in p[1].items()]
                 id = self.execute(
-                    f"insert into log(id,key,value) values "
-                    f"(coalesce((select max(id) from log), -1) + 1,{self.key_id(rows[0][1])},'{rows[0][2]}')"
+                    f"insert into log(id,key,value)values"
+                    f"(coalesce((select max(id)from log),-1)+1,{self.key_id(rows[0][1])},'{rows[0][2]}')"
                     "returning id"
                 ).__next__()[0]
                 buf.extend(f"({id},{self.key_id(r[1])},'{r[2]}')" for r in rows[1:])
-        self.execute(f"insert into log(id,key,value) values" + ",".join(buf))
-
-    def __hash__(self):
-        return hash(self.execute)
+        self.execute(f"insert into log(id,key,value)values" + ",".join(buf))
 
     @functools.cache
     def key_id(self, key: str) -> int:
-        try:
-            return next(self.execute_enum(f"select i from keys where key='{key}'"))[0]
-        except StopIteration:
-            return self.execute_enum(
-                f"insert into keys(key) values ('{key}') returning *"
-            ).__next__()[0]
+        if result := list(self.execute_enum(f"select i from keys where key='{key}'")):
+            return result[0][0]
+        return next(
+            self.execute_enum(f"insert into keys(key)values('{key}')returning *")
+        )[0]
 
     @functools.cache
     def id_key(self, id: int) -> str:
@@ -70,23 +63,15 @@ class Log:
         get: set[str] = set(),
     ):
         query = "select id,key,value from log where " + " and ".join(
-            itertools.chain(
-                (
-                    f"id in (select id from log where key={self.key_id(key)}"
-                    + (f" and value='{value}')" if value != True else ")")
-                    for key, value in present.items()
-                ),
-                (
-                    f"id not in (select id from log where key={self.key_id(key)}"
-                    + (f" and value='{value}')" if value != True else ")")
-                    for key, value in absent.items()
-                ),
-            )
+            f"id {clause} (select id from log where key={self.key_id(key)}"
+            + (f" and value='{value}')" if value != True else ")")
+            for clause, d in (("in", present), ("not in", absent))
+            for key, value in d.items()
         )
         if get:
-            query += "and (" + " or ".join(f"key={self.key_id(k)}" for k in get) + ")"
+            query += "and(" + " or ".join(f"key={self.key_id(k)}" for k in get) + ")"
         current = {}
-        for row in self.execute(query + " order by id,i"):
+        for row in self.execute(query + "order by id,i"):
             if "id" in current and current["id"] != row[0]:
                 yield current
                 current = {}
