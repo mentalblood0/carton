368b56d93cb17c90369e59a6b7208d5deed6da54
Нечепоренко Степан <neceporenkostepan@gmail.com>
2024-02-13 13:23:09 +0300

    add setup.py

---
+++ setup.py
@@ -0,0 +1,32 @@
+import pathlib
+
+import setuptools
+
+if __name__ == "__main__":
+    setuptools.setup(
+        name="log",
+        version="0.1.0",
+        python_requires=">=3.11",
+        keywords=["data-processing"],
+        url="https://github.com/MentalBlood/log",
+        description="Core for log driven data processing applications",
+        long_description=(pathlib.Path(__file__).parent / "README.md").read_text(),
+        long_description_content_type="text/markdown",
+        classifiers=[
+            "Development Status :: 3 - Alpha",
+            "Intended Audience :: Developers",
+            "Topic :: Database",
+            "Topic :: Software Development :: Libraries",
+            "Typing :: Typed",
+            "Topic :: System :: Logging",
+            "Operating System :: OS Independent",
+            "Programming Language :: Python :: 3.11",
+            "Programming Language :: Python :: 3.12",
+            "License :: OSI Approved :: BSD License",
+        ],
+        author="mentalblood",
+        author_email="neceporenkostepan@gmail.com",
+        maintainer="mentalblood",
+        maintainer_email="neceporenkostepan@gmail.com",
+        packages=setuptools.find_packages(exclude=["tests"]),
+    )
