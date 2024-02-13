ce8e595d2415a45ffe5c249a88a82bff87952e65
Нечепоренко Степан <neceporenkostepan@gmail.com>
2024-02-13 15:26:48 +0300

    make coverage 100%

--- Log/tests/test_basic.py
+++ Log/tests/test_basic.py
@@ -24,10 +24,12 @@ def test_benchmark_insert(
     benchmark: pytest_benchmark.plugin.BenchmarkFixture,
     amount: int,
 ):
-    def insert(log):
-        log.insert((None if i % 2 else i, {"key": f"value_{i}", "a": "b", "file": f"path_{i}"}) for i in range(amount))
-
-    benchmark.pedantic(insert, iterations=1, args=(log,))
+    benchmark.pedantic(
+        lambda: log.insert(
+            (None if i % 2 else i, {"key": f"value_{i}", "a": "b", "file": f"path_{i}"}) for i in range(amount)
+        ),
+        iterations=1,
+    )
 
 
 @pytest.mark.parametrize("amount", [10**n for n in range(5)])
@@ -38,22 +40,21 @@ def test_benchmark_select(
 ):
     log.insert((None if i % 2 else i, {"key": f"value_{i}", "a": "b", "file": f"path_{i}"}) for i in range(amount))
 
-    def select():
-        result = False
-        for p in log.select({"key": f"value_{amount-1}"}, {}, {"file", "a"}):
-            result = True
-            assert "package" in p
-            assert isinstance(p["package"], int)
-            assert "file" in p
-            assert isinstance(p["file"], str)
-            assert "a" in p
-            assert isinstance(p["a"], str)
-        assert result
-
     benchmark.pedantic(
-        select,
+        lambda: list(log.select({"key": f"value_{amount-1}"}, {}, {"file", "a"})),
         iterations=1,
     )
+
+    result = False
+    for p in log.select({"key": f"value_{amount-1}"}, {}, {"file", "a"}):
+        result = True
+        assert "package" in p
+        assert isinstance(p["package"], int)
+        assert "file" in p
+        assert isinstance(p["file"], str)
+        assert "a" in p
+        assert isinstance(p["a"], str)
+    assert result
 
 
 def test_groupby(log: Log):
@@ -64,6 +65,7 @@ def test_groupby(log: Log):
     result = list(log.select({"x": "y"}))
     assert {"package": 0, "a": "b", "e": "f", "x": "y"} in result
     assert {"package": 1, "c": "d", "g": "h", "x": "y"} in result
+    assert list(log.select({"x": "y"})) == list(log.select({"x": "y"}, get={"package", "a", "e", "c", "g", "x"}))
 
 
 def test_distinct(log: Log):
