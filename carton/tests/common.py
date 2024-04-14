fab5609cc29f31be28b1a9c4b8e95001205c6d31
mentalblood <neceporenkostepan@gmail.com>
2024-04-14 14:39:25 +0300

    flake8 related fix

--- carton/tests/common.py
+++ carton/tests/common.py
@@ -1,14 +1,16 @@
 import sqlite3
 
+import psycopg2
 import pytest
 
 from .. import databases
 from ..Carton import Carton
 
 
-@pytest.fixture()
+@pytest.fixture
 def carton():
-    db = databases.Sqlite(sqlite3.connect(":memory:"))
+    # db = databases.Sqlite(sqlite3.connect(":memory:"))
+    db = databases.Postgres(psycopg2.connect(database="db1", password="fonofme"))
     cursor = db.cursor()
     cursor.execute("drop table if exists sentences")
     cursor.execute("drop table if exists predicates")
