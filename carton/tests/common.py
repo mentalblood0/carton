2e678321694f35f56b13cb7d6d05f65e2b80f830
mentalblood <neceporenkostepan@gmail.com>
2024-04-14 14:43:36 +0300

    sqlite3 related testing fixes

--- carton/tests/common.py
+++ carton/tests/common.py
@@ -1,6 +1,5 @@
 import sqlite3
 
-import psycopg2
 import pytest
 
 from .. import databases
@@ -9,10 +8,5 @@ from ..Carton import Carton
 
 @pytest.fixture
 def carton():
-    # db = databases.Sqlite(sqlite3.connect(":memory:"))
-    db = databases.Postgres(psycopg2.connect(database="db1", password="fonofme"))
-    cursor = db.cursor()
-    cursor.execute("drop table if exists sentences")
-    cursor.execute("drop table if exists predicates")
-    db.commit()
+    db = databases.Sqlite(sqlite3.connect(":memory:"))
     return Carton(db)
